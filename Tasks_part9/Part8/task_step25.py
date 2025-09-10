8b638558480006cb2d0201ea4a0742fb7adf297e
gigcomm <lenovo27835@gmail.com>
2025-09-10 18:33:07 +0300

    Add tasks

--- Tasks_part9/Part8/task_step25.py
+++ Tasks_part9/Part8/task_step25.py
@@ -9,7 +9,7 @@ def retry(times):
         def wrapper(*args, **kwargs):
             for _ in range(times):
                 try:
-                    func(*args, **kwargs)
+                    return func(*args, **kwargs)
                 except Exception:
                     pass
             raise MaxRetriesException
@@ -17,12 +17,12 @@ def retry(times):
     return decorator
 
 
-@retry(3)
-def no_way():
-    raise ValueError
+@retry(8)
+def beegeek():
+    beegeek.calls = beegeek.__dict__.get('calls', 0) + 1
+    if beegeek.calls < 5:
+        raise ValueError
+    print('beegeek')
 
 
-try:
-    no_way()
-except Exception as e:
-    print(type(e))
+beegeek()
