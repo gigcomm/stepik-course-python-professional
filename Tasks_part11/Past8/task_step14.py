40721b322d49c7ab5086fda4164d91ee588e672d
gigcomm <lenovo27835@gmail.com>
2025-09-29 14:51:01 +0300

    Add tasks

--- Tasks_part11/Past8/task_step14.py
+++ Tasks_part11/Past8/task_step14.py
@@ -1,29 +1,9 @@
 import re
-import sys
 
-code = sys.stdin.read()
+text = input().strip()
 
-code = re.sub(r'""".*?"""', '', code, flags=re.DOTALL)
+pattern = r'(\b\w+\b)(\W+\1\b)+'
+res = re.sub(pattern, r'\1', text)
+print(res)
 
-code = re.sub(r'^\s*#.*$', '', code, flags=re.MULTILINE)
-
-code = re.sub(r'#.*$', '\n', code, flags=re.MULTILINE)
-
-lines = [line.rstrip() for line in code.splitlines() if line.strip() != '']
-
-print('\n'.join(lines))
-
-# """hello everyone
-# welcome to my project"""
-#
-# import math  # importing a useful math module
-# # let's take a look at some functions
-# def circle_area(radius):
-#     """coming soon"""
-#     return math.pi * r ** 2  # my favorite formula
-# def triangle_area(base, height):
-#     """the function takes
-#     the base and height
-#     of a triangle and
-#     returns its area"""
-#     return 0.5 * base * height
+#hi, hi, hi, hello, hello, HELLO, HELLO, HELLO, HELLO, hello!
